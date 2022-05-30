42e296f3db37c10b3e5558344802872753a25a64
richardier <ianis.richardier@live.fr>
2022-05-30 11:04:36 +0200

    Initial commit

---
+++ WeatherstackApp/ApiKey.swift
@@ -0,0 +1,8 @@
+//
+//  ApiKey.swift
+//  WeatherstackApp
+//
+//  Created by Richardier on 30/05/2022.
+//
+
+import Foundation
---
+++ WeatherstackApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherstackApp
+//
+//  Created by Richardier on 30/05/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
